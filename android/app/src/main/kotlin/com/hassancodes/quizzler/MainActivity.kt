c5bdff54b3f689754cfca4be5ca6ed16bbbdade3
Baako Alhassan Mohammed <baakomohammed95@gmail.com>
2021-11-07 01:54:59 +0000

    Initialize project

---
+++ android/app/src/main/kotlin/com/hassancodes/quizzler/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hassancodes.quizzler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Quizzler());
+}
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
