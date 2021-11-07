3f2f65935a70596913ffd8189ca35cc4b2c09b30
Baako Alhassan Mohammed <baakomohammed95@gmail.com>
2021-11-07 21:17:11 +0000

    Create answers list

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class _QuizPageState extends State<QuizPage> {
     'Approximately one quarter of human bones are in the feet.',
     'A slug\'s blood is green.'
   ];
+  List<bool> answers = [false, true, true];
   int questionNumber = 0;
 
   @override
