f3a785562716e48d4579146876659b0bf31700ac
Baako Alhassan Mohammed <baakomohammed95@gmail.com>
2021-11-14 21:33:21 +0000

    Optimize Question class

--- lib/question.dart
+++ lib/question.dart
@@ -2,8 +2,5 @@ class Question {
   late String questionText;
   late bool questionAnswer;
 
-  Question(String q, bool a) {
-    questionText = q;
-    questionAnswer = a;
-  }
+  Question(this.questionText, this.questionAnswer);
 }
